1f8ff8528f4d740fe3dcad2ded4a2284aa477c3e
abdulshakoor123 <hangu3456789@gmail.com>
2024-08-18 12:48:50 +0500

    added file

---
+++ function-19.js
@@ -0,0 +1,45 @@
+Function exerices
+// function giveSquare(num) {
+//     return num * num;
+// }
+
+// let result = giveSquare(4);
+// console.log(result);
+
+// // 2
+
+// let count = 10;
+// function locanVar() {
+// let localVar = 9;
+// localVar * 2;
+// return localVar
+// }
+
+// // console.log(`local var ${localVar}`);
+// // console.log(`global variable ${count}`);
+
+// // // 3
+// var totalNumber = 34;
+// function totalNum(num) {
+//     totalNumber = totalNumber + num;
+//     return totalNumber;
+// }
+// totalNumber(6);
+
+// console.log(totalNum(6));
+
+// // 4
+// function greetUser(name) {
+//     return `hello ${name}`
+
+// }
+// console.log(greetUser("alice"));
+
+// // 5
+// var a = "inactive";
+// function activation() {
+//     let Ab = "active"
+//     return Ab;
+// }
+// console.log(activation());
+// console.log(a);
